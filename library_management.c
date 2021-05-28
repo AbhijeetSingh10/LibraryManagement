86b7a25d3799ce72a78157a98300120c8bf98f9f
Abhijeet Singh <71282822+AbhijeetSingh10@users.noreply.github.com>
2021-05-28 16:32:54 +0530

    My first commit

---
+++ library_management.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<Stdlib.h>
+
+typedef struct
+{
+    char username[15];
+    char password[15];
+} User;
+
+typedef struct
+{
+    int id;
+    char title[50];
+    char author[50];
+    char category[50];
+    char publicaton[50];
+    char description[500];
+    int taken;
+
+} Book;
+
+int main()
+{
+
+}
